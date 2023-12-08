cf02b2f821d1778ae0e9d5c9ecdf0fbb5061e39b
Evgeny <knyazev.evgeny@gmail.com>
2023-12-08 15:09:26 +0400

    resolved next

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val linearLayout = LinearLayout(this)
+        val textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.text = "I am an Android Developer!"
+        linearLayout.addView(textView)
+        val button = Button(this)
+        button.id = R.id.button
+        linearLayout.addView(button)
+        setContentView(linearLayout)
     }
 }
